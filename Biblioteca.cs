328b4ed9c3f6160dc518c02533ef407a642851ad
mariabuelvas21 <112511200+mariabuelvas21@users.noreply.github.com>
2022-09-08 11:41:10 -0500

    Add files via upload

---
+++ Biblioteca.cs
@@ -0,0 +1,43 @@
+﻿//clase principal
+public class Biblioteca{
+    //metodo principal
+    public static void Main(string[] args){
+        //defino una instancia de libro como odisea, y la inicializo como nueva
+        libro Odisea=new libro();
+         //defino una instancia de libro como dracula, y la inicializo como nueva
+         libro Dracula=new libro();
+       
+       //llama al configurador de autor, y le damos el valor Homero
+        Odisea.setAutor("Homero");
+         //llama al configurador de Npaginas, y le damos el valor 448
+        Odisea.setNpaginas(448);
+         //llama al configurador de ISBN, y le damos el valor 9789583043888
+        Odisea.setISBN(9789583043888);
+         //llama al configurador de Titulo, y le damos el valor Odisea
+        Odisea.setTitulo("Odisea");
+        //llama al metodo tostring, y muestra el contenido de la clase libro agrgandole los valores que le dimos a Odisea
+        Odisea.tostring();
+
+        //llama al configurador de autor, y le damos el valor Bram Stoker
+        Dracula.setAutor("Bram Stoker");
+        //llama al configurador de Npaginas, y le damos el valor 418
+        Dracula.setNpaginas(418);
+        //llama al configurador de ISBN, y le damos el valor 9780786181483
+        Dracula.setISBN(9780786181483);
+        //llama al configurador de Titulo, y le damos el valor Dracula
+        Dracula.setTitulo("Dracula");
+        //llama al metodo tostring, y muestra el contenido de la clase libro agrgandole los valores que le dimos a Dracula
+        Dracula.tostring();
+    
+      //condicion Si, para saber cual de los dos libros tiene mas pagina
+        if (Odisea.getNpaginas()>Dracula.getNpaginas()){
+            //si la condicion se cumple, muetra en pantalla que odisea tiene mas paginas 
+            Console.WriteLine("Odisea tiene mas paginas");
+        }
+        //SiNo para cuando no se cumple la condicion 
+        else {
+            //muestra en pantalla que la Dracula tiene mas paginas 
+            Console.WriteLine("Dracula tiene mas paginas");
+        }
+    }
+}
---
+++ Libro.cs
@@ -0,0 +1,55 @@
+﻿// clase publica del libro
+public class libro{
+   //atributo privado para guardar el autor y el titulo del libro 
+  string Autor="",Titulo="";
+  //atributo privado para guardar el nuemero de pagina del libro
+  int Npaginas;
+//atributo privado para guardar el ISBN del libro
+  long ISBN;
+  
+  //metodo publico de tipo string para consultar el autor
+  public string getAutor(){
+    //funcion que devuelve el valor y llama al autor
+    return this.Autor;
+  }
+  //metodo publico que configura al autor y la guarda en la variable nombre de tipo string
+  public void setAutor(string nombre){
+    //procedimiento que llama al autor, que se guardo en variable nombre
+    this.Autor=nombre;
+  }
+  //metodo publico de tipo string para consultar el titulo
+  public string getTitulo(){
+    //funcion que devuelve el valor y llama al titulo
+    return this.Titulo;
+  }
+  //metodo publico que configura al titulo y la guarda en la variable titular de tipo string
+  public void setTitulo(string titular){
+    //procedimiento que llama al titulo, que se guardo en variable titular
+    this.Titulo=titular;
+  }
+   //metodo publico de tipo int para consultar el Npaginas
+  public int getNpaginas(){
+    //funcion que devuelve el valor y llama al Npaginas
+    return this.Npaginas;
+  }
+  //metodo publico que configura al Npaginas y la guarda en la variable paginas de tipo int
+  public void setNpaginas(int paginas){
+    //procedimiento que llama al Npaginas, que se guardo en variable paginas
+    this.Npaginas=paginas;
+  }
+   //metodo publico de tipo long para consultar el ISBN
+  public long getISBN(){
+    //funcion que devuelve el valor y llama al ISBN
+    return this.ISBN;
+  }
+  //metodo publico que configura al ISBN y la guarda en la variable Nbarras de tipo long
+  public void setISBN(long Nbarras){
+    //procedimiento que llama al ISBN, que se guardo en variable Nbarras
+    this.ISBN=Nbarras;
+  }
+  //metodo publico tostring que muestra el contenido de la clase
+  public void tostring(){
+    //muestra en pantalla el libro  con IBSN  creado por el autor  tiene  paginas
+    Console.WriteLine("El libro "+this.Titulo+" con ISBN "+this.ISBN+" creado por el autor "+this.Autor+" tiene "+this.Npaginas+" páginas.");
+  }
+}
